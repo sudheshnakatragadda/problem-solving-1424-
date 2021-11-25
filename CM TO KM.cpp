6452a7df1eda07d619eb7266b396d7d1395271f1
sudheshnakatragadda <93900952+sudheshnakatragadda@users.noreply.github.com>
2021-11-25 15:32:56 +0530

    Add files via upload

---
+++ CM TO KM.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a value a in cm");
+	scanf("%d",&a);
+	printf("enter value b in km");
+	scanf("%d",&b);
+	b=a*1/100000;
+	return 0;
+}
+	
