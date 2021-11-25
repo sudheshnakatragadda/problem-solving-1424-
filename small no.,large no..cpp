72870640b4aad922fc7b043a7ff5420b21b3ffb6
sudheshnakatragadda <93900952+sudheshnakatragadda@users.noreply.github.com>
2021-11-25 15:41:10 +0530

    Add files via upload

---
+++ small no.,large no..cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a number a");
+    scanf("%d",&a);
+    printf("enter a number b");
+    scanf("%d",&b);
+    if (a>b)
+    {
+    printf("a is the biggest number");
+    }
+    else
+    {
+    printf("b is the biggest number");
+    }
+    return 0;
+    
+}
